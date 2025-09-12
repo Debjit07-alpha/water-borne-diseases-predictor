fca47c7bcff24313e19bc9cc96c631f4968dbc99
Debjit Saha <sahadebjit07@gmail.com>
2025-09-12 05:47:15 +0530

    refactor: update Tailwind CSS configuration for improved content paths and container settings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,13 +3,18 @@ import type { Config } from "tailwindcss";
 export default {
   darkMode: ["class"],
   content: [
-    "./pages//*.{js,ts,jsx,tsx,mdx}",
-    "./components//*.{js,ts,jsx,tsx,mdx}",
-    "./app//*.{js,ts,jsx,tsx,mdx}",
-    "./src//*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
 
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       colors: {
         primary: '#0ea5e9',
